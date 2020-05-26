17dcb1afd1a5f106f5970f57105ca7200986b1bb
Your Name <social@coderwelsch.com>
2020-05-26 13:14:55 +0200

    Fixed webfont generator problem while exporting scss and html files

--- src/node/export-icon-font.js
+++ src/node/export-icon-font.js
@@ -1,20 +1,27 @@
 const iconfontMaker = require("iconfont-maker");
-
+const FS = require("fs");
+const Path = require("path");
 const args = process.argv.slice(2);
 
 
 iconfontMaker({
+	fontName: "icon",
 	files: [
 		`${ args[0] }**/*.svg`
 	],
 	dest: args[1],
-	fontName: "icon",
-	html: true,
+
 	css: true,
-	cssDest: args[1] + "styles.css",
-	htmlDest: args[1] + "index.html"
+	cssDest: args[1] + "index.scss",
+	cssTemplate: Path.resolve(process.cwd(), "src/node/templates/scss.hbs"),
+	cssFontsUrl: "#{ $icon-font-base-path }",
+
+	html: true,
+	htmlDest: args[1] + "index.html",
+	htmlTemplate: Path.resolve(process.cwd(), "src/node/templates/html.hbs"),
 }, function (error) {
 	if (error) {
+		console.error(error);
 		throw error;
 	}
 });
